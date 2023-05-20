a58f2b3c84034addca6b5b682f1062c9ea869ba2
cyrilmuchemi <cyrilmuchemi@gmail.com>
2023-05-20 19:46:15 +0300

    Create destination page

---
+++ src/components/Europa.vue
@@ -0,0 +1,16 @@
+<script>
+import sourceData from '@/data.json'
+
+export default {
+    data() {
+        return {
+            name: sourceData.destinations[2].name
+        }
+    }
+}
+
+</script>
+
+<template>
+<h1 class="text-white fs-900 ff-sans-cond">{{name}}</h1>
+</template>
--- src/views/Destination.vue
+++ src/views/Destination.vue
@@ -1,16 +1,33 @@
 <template>
-    <div class="destination">
-        <h1>Destination</h1>
+    <div class="destination text-white">
+        <main id="main" class="grid-container  grid-container--destination">
+            <h1 class="numbered-title"><span aria-hidden="true">01</span>Pick your destination</h1>
+            <img src="../assets/destination/image-moon.png" alt="the moon"/>
+
+            <div class="tab-list underline-indicators flex">
+                <button aria-selected="true" class="uppercase ff-sans-cond text-accent bg-dark letter-spacing-2">Moon</button>
+                <button aria-selected="false" class="uppercase ff-sans-cond text-accent bg-dark letter-spacing-2">Mars</button>
+                <button aria-selected="false" class="uppercase ff-sans-cond text-accent bg-dark letter-spacing-2">Europa</button>
+                <button aria-selected="false" class="uppercase ff-sans-cond text-accent bg-dark letter-spacing-2">Titan</button>
+            </div>
+
+    <article class="destination-info">
+      <h2 class="fs-800 uppercase ff-serif">Moon</h2>
+      <p>See our planet as you’ve never seen it before. A perfect relaxing trip away to help 
+      regain perspective and come back refreshed. While you’re there, take in some history 
+      by visiting the Luna 2 and Apollo 11 landing sites.</p>   
+      <div class="destination-meta flex">
+      <div>
+        <h3>Avg. distance</h3>
+        <p class="fs-500 ff-serif uppercase">384,400 km</p>
+      </div>
+      <div>
+        <h3>Est. travel time</h3>
+        <p class="fs-500 ff-serif uppercase">3 days</p>
+      </div>
+      </div>
+    </article>
+        </main>
     </div>
 </template>
 
-<script>
-import sourceData from '@/data.json'
-export default {
-    data() {
-        return {
-           destinations: sourceData.destinations
-        }
-    }
-}
-</script>
